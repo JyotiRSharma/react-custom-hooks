5c52cede15980e48d32cf76f50c9966a364cf5dd
jyoti ranjan sharma <jyotiranjansharma11@gmail.com>
2023-09-08 21:54:20 +0530

    Add useDocTitle custom hook

--- src/components/DocTitleTwo.tsx
+++ src/components/DocTitleTwo.tsx
@@ -1,18 +1,19 @@
-import { useEffect, useState } from "react"
+import { useState } from "react";
+import useDocTitle from "../hooks/useDocTitle";
 
 const DocTitleTwo = () => {
-    const [count, setCount] = useState(0);
-    useEffect(() => {
-        document.title = `Count ${count}`;
-    })
-    return (
-        <>
-        <div>
-            <h1>Lets make the title count again!</h1>
-            <button onClick={() => setCount(count+1)}>Doc title one {count}</button>
-        </div>
-        </>
-    )
-}
+  const [count, setCount] = useState(0);
+  useDocTitle(count);
+  return (
+    <>
+      <div>
+        <h1>Lets make the title count again!</h1>
+        <button onClick={() => setCount(count + 1)}>
+          Doc title one {count}
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default DocTitleTwo;
